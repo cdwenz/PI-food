145170d9a6b0e21da330f8b34fcf1541729488ba
cristian <cdwenz@gmail.com>
2021-10-17 22:30:43 -0300

    pre-deploy

--- api/index.js
+++ api/index.js
@@ -3,20 +3,21 @@ const { conn, Diet } = require('./src/db.js');
 const {preload} = require('./src/Components/Loader')
 
 // Syncing all the models at once.
-// conn.sync({ force: false }).then(() => {
-//   server.listen(3001, () => {
-//     console.log('listening at 3001'); // eslint-disable-line no-console
-//   });
-// });
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('listening at ' + PORT); // eslint-disable-line no-console
+  });
+});
+Diet.findAll().then(e => preload(e))
 
 //loader DIETS
-const serverUp = async () => {
-  await conn.sync({ force: false })
-    console.log('DB connected')
-  await Diet.findAll().then(e => preload(e))
-  await  server.listen(process.env.PORT, () => {
-    console.log('listening at 3001'); 
-  });
-}
+// const serverUp = async () => {
+//   await conn.sync({ force: false })
+//     console.log('DB connected')
+//   await Diet.findAll().then(e => preload(e))
+//   await  server.listen(process.env.PORT, () => {
+//     console.log('listening at 3001'); 
+//   });
+// }
 
 serverUp();
