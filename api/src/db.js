90e458324c811a8d2450a5cfc9ccf69dbfe501bd
cristian <cdwenz@gmail.com>
2021-10-22 13:29:28 -0300

    dotenv

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if(process.env.NODE_ENV !== "production") require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
